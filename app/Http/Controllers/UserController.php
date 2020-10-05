3f81db0543d43d460e18ec4e39115c907738ddc0
hiroki0276 <g9daf788e7qcn@gmail.com>
2020-10-05 14:00:26 +0900

    細々した修正
    N+1問題解決

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,7 +9,8 @@ class UserController extends Controller
 {
     public function show(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+            ->load(['articles.user', 'articles.likes', 'articles.tags']);
         $articles = $user->articles->sortByDesc('created_at');
 
         return view('users.show', [
@@ -19,8 +20,8 @@ class UserController extends Controller
     }
     public function likes(string $name)
     {
-        $user = User::where('name', $name)->first();
-
+        $user = User::where('name', $name)->first()
+            ->load(['likes.user', 'likes.likes', 'likes.tags']);
         $articles = $user->likes->sortByDesc('created_at');
 
         return view('users.likes', [
@@ -30,8 +31,8 @@ class UserController extends Controller
     }
     public function followings(string $name)
     {
-        $user = User::where('name', $name)->first();
-
+        $user = User::where('name', $name)->first()
+            ->load('followings.followers');
         $followings = $user->followings->sortByDesc('created_at');
 
         return view('users.followings', [
@@ -42,8 +43,8 @@ class UserController extends Controller
     
     public function followers(string $name)
     {
-        $user = User::where('name', $name)->first();
-
+        $user = User::where('name', $name)->first()
+            ->load('followers.followers');
         $followers = $user->followers->sortByDesc('created_at');
 
         return view('users.followers', [
